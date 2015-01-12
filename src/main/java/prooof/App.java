201acc609197d9c32d6e3c5c45b9e4f6e77a1933
Michael Bumann <michael@railslove.com>
2015-01-12 18:59:45 +0100

    hello world

---
+++ src/main/java/prooof/App.java
@@ -0,0 +1,144 @@
+package prooof;
+
+import static spark.Spark.before;
+import static spark.Spark.get;
+import static spark.Spark.post;
+import static spark.SparkBase.setPort;
+
+import java.io.File;
+import java.util.List;
+
+import org.bitcoinj.core.Transaction;
+import org.bitcoinj.core.TransactionOutput;
+import org.bitcoinj.core.Wallet;
+import org.bitcoinj.script.Script;
+import org.json.simple.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.kevinsawicki.http.HttpRequest;
+import com.google.common.io.BaseEncoding;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        Logger logger = LoggerFactory.getLogger(App.class.getName());
+        logger.info("Nice to see you!");
+
+        String environment = System.getenv("BITCOIN_NETWORK");
+        if (environment == null) {
+            environment = "org.bitcoin.test";
+        }
+        logger.info("using network " + environment);
+
+        String walletPath = System.getenv("WALLET_PATH");
+        if (walletPath == null) {
+            walletPath = "./main.wallet";
+        }
+        logger.info("loading wallet from: " + walletPath);
+
+        String rootDir = System.getenv("ROOT_DIR");
+        if (rootDir == null) {
+            rootDir = "./";
+        }
+        logger.info("using root directory: " + rootDir);
+
+        String localhost = System.getenv("CONNECT_TO_LOCALHOST");
+        boolean useLocalhost = localhost != null && localhost.equals("1");
+
+        final Treasury treasury = new Treasury(environment, new File(rootDir), useLocalhost);
+        treasury.loadWalletFromFile(new File(walletPath));
+        treasury.start();
+
+        logger.info("currenct receive address: " + treasury.wallet.currentReceiveAddress());
+
+        String port = System.getenv("PORT");
+        if (port != null) {
+            setPort(Integer.parseInt(port));
+        }
+
+        before("/*", (request, response) -> {
+            logger.info(request.requestMethod() + " " + request.pathInfo() + " ip=" + request.ip());
+        });
+
+        get("/", (req, res) -> {
+            return "ping " + new java.util.Date().getTime();
+        });
+
+        post("/api/sign", (req, res) -> {
+            String data = req.queryParams("data");
+            String notificationUrl = req.queryParams("notification_url");
+            logger.info("data: " + data + " notification_url: " + notificationUrl);
+            JSONObject responseJson = new JSONObject();
+            try {
+                Wallet.SendRequest sendRequest = treasury.createAndPublishOpReturn(data);
+                if (notificationUrl != null && notificationUrl != "") {
+                    logger.info("registering future callback");
+                    Futures.addCallback(sendRequest.tx.getConfidence().getDepthFuture(1), new FutureCallback<Transaction>() {
+                        @Override
+                        public void onSuccess(Transaction result) {
+                            logger.info("one confirmation for " + result.getHashAsString());
+                            HttpRequest request = HttpRequest.post(notificationUrl);
+                            request.header("Content-Type", "application/json");
+                            JSONObject txJson = new JSONObject();
+                            txJson.put("hash", result.getHashAsString());
+                            txJson.put("confidence", result.getConfidence().getDepthInBlocks());
+                            txJson.put("appearsIn", result.getConfidence().getAppearedAtChainHeight());
+                            request.send(txJson.toJSONString());
+                            if (request.ok()) {
+                                logger.info("notification successful for transaction " + result.getHashAsString());
+                            } else {
+                                logger.info("notification failed for transaction " + result.getHashAsString());
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Throwable t) {
+                            // This kind of future can't fail, just rethrow in case something very weird happens.
+                            throw new RuntimeException(t);
+                        }
+                    });
+                }
+                responseJson.put("hash", sendRequest.tx.getHashAsString());
+            } catch (Exception e) {
+                logger.error("insufficient money", e);
+                responseJson.put("error", "enough money?");
+                res.status(422);
+            }
+            res.type("application/json");
+            return responseJson.toJSONString();
+        });
+
+        post("/api/validate/:txHash", (req, res) -> {
+            String expectedData = req.queryParams("data");
+            String txHash = req.params("txHash");
+            logger.info("validating " + txHash);
+            Transaction tx;
+            try {
+                tx = treasury.fetchTransaction(txHash);
+                List<TransactionOutput> outputs = tx.getOutputs();
+                for (TransactionOutput output : outputs) {
+                    Script script = output.getScriptPubKey();
+                    if (script.isOpReturn()) {
+                        byte[] hexData = script.getChunks().get(0).data;
+                        String hexDataStr = new String(hexData);
+
+                        byte[] txData = BaseEncoding.base32Hex().decode(hexDataStr);
+                        String txDataStr = new String(txData);
+
+                        logger.info("hexDataStr" + hexDataStr);
+                        logger.info("txDataStr" + txDataStr);
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                logger.error("error getting the transaction", e);
+            }
+
+            res.type("application/json");
+            return "{}";
+        });
+    }
+}
---
+++ src/main/java/prooof/Treasury.java
@@ -0,0 +1,166 @@
+package prooof;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.concurrent.TimeUnit;
+
+import org.bitcoinj.core.AbstractWalletEventListener;
+import org.bitcoinj.core.BlockChain;
+import org.bitcoinj.core.Coin;
+import org.bitcoinj.core.DownloadListener;
+import org.bitcoinj.core.InsufficientMoneyException;
+import org.bitcoinj.core.NetworkParameters;
+import org.bitcoinj.core.Peer;
+import org.bitcoinj.core.PeerAddress;
+import org.bitcoinj.core.PeerGroup;
+import org.bitcoinj.core.Sha256Hash;
+import org.bitcoinj.core.Transaction;
+import org.bitcoinj.core.Wallet;
+import org.bitcoinj.core.Wallet.SendRequest;
+import org.bitcoinj.net.discovery.DnsDiscovery;
+import org.bitcoinj.script.Script;
+import org.bitcoinj.script.ScriptBuilder;
+import org.bitcoinj.store.SPVBlockStore;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+public class Treasury {
+    public NetworkParameters params;
+    public static String opReturnIdentifier = "PROOOF1-";
+    public Wallet wallet;
+    private File chainFile;
+    private SPVBlockStore chainStore;
+    private BlockChain blockChain;
+    private PeerGroup peerGroup;
+    public File walletFile;
+    private boolean useLocalhost;
+    static Logger logger = LoggerFactory.getLogger(Treasury.class.getName());
+
+    public Treasury(String environment) throws Exception {
+        this(environment, false);
+    }
+
+    public Treasury(String environment, boolean useLocalhost) throws Exception {
+        this(environment, new File("."), useLocalhost);
+    }
+
+    public Treasury(String environment, File directory, boolean useLocalhost) throws Exception {
+        this.params = this.paramsForEnvironment(environment);
+        this.chainFile = new File(directory, "prooof-" + environment + ".spvchain");
+        this.useLocalhost = useLocalhost;
+        this.chainStore = new SPVBlockStore(params, this.chainFile);
+        this.blockChain = new BlockChain(params, this.chainStore);
+        this.peerGroup = new PeerGroup(params, this.blockChain);
+    }
+
+    public void loadWalletFromFile(File walletFile) throws Exception {
+        this.walletFile = walletFile;
+        if (walletFile.exists()) {
+            this.wallet = Wallet.loadFromFile(this.walletFile);
+        } else {
+            this.wallet = new Wallet(this.params);
+            this.wallet.saveToFile(walletFile);
+        }
+        this.wallet.autosaveToFile(this.walletFile, 300, TimeUnit.MILLISECONDS, null);
+        this.wallet.addEventListener(new Treasury.WalletListener());
+        this.blockChain.addWallet(this.wallet);
+        this.peerGroup.addWallet(this.wallet);
+    }
+
+    public void start() throws Exception {
+        if (this.useLocalhost) {
+            InetAddress localhost = InetAddress.getLocalHost();
+            PeerAddress localPeer = new PeerAddress(localhost, this.params.getPort());
+            this.peerGroup.addAddress(localPeer);
+        } else {
+            this.peerGroup.addPeerDiscovery(new DnsDiscovery(this.params));
+        }
+
+        DownloadListener bListener = new DownloadListener() {
+            @Override
+            public void doneDownload() {
+                logger.info("blockchain downloaded");
+            }
+        };
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                try {
+                    logger.info("shutting down");
+                    wallet.saveToFile(walletFile);
+                    logger.info("saved all wallets, BYE");
+                } catch (Exception e) {
+                    logger.error("error saving wallet");
+                }
+            }
+        });
+        this.peerGroup.startAsync();
+        this.peerGroup.awaitRunning();
+        this.peerGroup.startBlockChainDownload(bListener);
+        //bListener.await();
+    }
+
+    public boolean save() {
+        try {
+            this.wallet.saveToFile(this.walletFile);
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    public Wallet.SendRequest createAndPublishOpReturn(String data) throws InsufficientMoneyException {
+        String opData = Treasury.opReturnIdentifier + data;
+        Transaction tx = new Transaction(this.params);
+        Script script = ScriptBuilder.createOpReturnScript(opData.getBytes());
+        tx.addOutput(Coin.ZERO, script);
+        SendRequest req = Wallet.SendRequest.forTx(tx);
+
+        logger.info("completing transaction");
+        this.wallet.completeTx(req);
+        logger.info("committing transaction");
+        this.wallet.maybeCommitTx(req.tx);
+        logger.info("broadcasting transaction");
+        this.peerGroup.broadcastTransaction(req.tx);
+        return req;
+    }
+
+    public Transaction fetchTransaction(String txHash) {
+        Peer peer = this.peerGroup.getConnectedPeers().get(0);
+        Sha256Hash txShaHash = new Sha256Hash(txHash);
+        ListenableFuture<Transaction> future = peer.getPeerMempoolTransaction(txShaHash);
+        logger.info("Waiting for node to send us the requested transaction: " + txHash);
+
+        ;
+        try {
+            Transaction tx = future.get(1, TimeUnit.SECONDS);
+            return tx;
+        } catch (Exception e) {
+            logger.error("faild to get transaction: " + txHash, e);
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private NetworkParameters paramsForEnvironment(String networkId) {
+        if (networkId == null) {
+            networkId = NetworkParameters.ID_TESTNET;
+        }
+        return NetworkParameters.fromID(networkId);
+    }
+
+    static class WalletListener extends AbstractWalletEventListener {
+
+        @Override
+        public void onCoinsReceived(Wallet wallet, Transaction tx, Coin prevBalance, Coin newBalance) {
+            logger.info("received transaction: " + tx.getHashAsString() + " value: " + tx.getValue(wallet));
+        }
+
+        @Override
+        public void onCoinsSent(Wallet wallet, Transaction tx, Coin prevBalance, Coin newBalance) {
+            logger.info("sent transaction: " + tx.getHashAsString() + " new balance: " + newBalance.toFriendlyString() + "BTC");
+        }
+    }
+}
